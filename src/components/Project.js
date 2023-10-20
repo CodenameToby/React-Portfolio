dd31802a401573f65f88b7a315b71cbba6aba1a7
Toby Warburton <tobywarburton10@gmail.com>
2023-10-20 11:44:26 -0700

    further updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,10 @@ export default function Test() {
     return (
       <div>
         <h1>Test</h1>
-        
+        <p>
+        <img src= "./public/assets/weatherApp.jpg" alt='Project Screenshot'> 
+        </img>
+      </p>
       </div>
     );
   }
